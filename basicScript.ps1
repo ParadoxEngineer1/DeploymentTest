7a4f7406e5f2ae9f6ac091912cb2ceb21e411ee6
ParadoxEngineer1 <jedwards@vtechsupport.com>
2025-03-07 11:30:45 -0800

    Create basicScript.ps1
    Created v0.1 with workstation rename and final shutdown command.

---
+++ basicScript.ps1
@@ -0,0 +1,20 @@
+#basicScript.ps1
+#v0.1 - 3/7/2025
+#Created by Jonathan Edwards
+#Purpose is to collect and run basic scripts that should be run on all workstation inedependent of 
+#company or parameters. Then to ask for company and call further scripts.
+
+#Rename Workstation
+$serialTag = (Get-WmiObject -class win32_bios).SerialNumber
+
+if (((Get-Computerinfo).CsPCSystemType) -eq "Desktop")
+{
+	Rename-Computer -NewName ("D" + $serialTag + "-D")
+}
+else
+{
+	Rename-Computer -New Name ("D" + $serialTag + "-L")
+}
+
+#Final shutdown to mark script completion.
+shutdown /s /t 60
