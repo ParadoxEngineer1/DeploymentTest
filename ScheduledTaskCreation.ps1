ed4d8695e2c94cb1297db26f4291746ab22426b7
ParadoxEngineer1 <jedwards@vtechsupport.com>
2025-03-20 16:32:11 -0700

    Create ScheduledTaskCreation.ps1
    Created v0.1.

---
+++ ScheduledTaskCreation.ps1
@@ -0,0 +1,15 @@
+#StartUpScript.ps1
+#v0.1 - 3/20/2025
+#Created by Jonathan Edwards
+#Creates Scheduled Task that run on system startup.
+
+#Create Scheduled Task
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"C:\StartUpScript.ps1`""
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+$settings = New-ScheduledTaskSettingsSet
+$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
+Register-ScheduledTask Deployment -InputObject $task
+
+#Download StartUpScript.ps1
+#Invoke-WebRequest -Uri https://.com\StartUpScript.ps1 -OutFile "C:\StartUpScript.ps1"
