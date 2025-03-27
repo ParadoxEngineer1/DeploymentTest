94a865c858e1f3ad3e79b56b231b6d9f3f11bad4
ParadoxEngineer1 <jedwards@vtechsupport.com>
2025-03-27 13:07:52 -0700

    Update ScheduledTaskCreation.ps1
    v0.1.2 Added running the StartUpScript

--- ScheduledTaskCreation.ps1
+++ ScheduledTaskCreation.ps1
@@ -1,5 +1,5 @@
 #StartUpScript.ps1
-#v0.1.1 - 3/20/2025
+#v0.1.2 - 3/27/2025
 #Created by Jonathan Edwards
 #Creates Scheduled Task that run on system startup.
 
@@ -13,3 +13,6 @@ Register-ScheduledTask Deployment -InputObject $task
 
 #Download StartUpScript.ps1
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/ParadoxEngineer1/DeploymentTest/refs/heads/main/StartUpScript.ps1 -OutFile "C:\StartUpScript.ps1"
+
+#Run StartUpScript.ps1
+& "C:\StartUpScript.ps1"
