d35f70d3bafe725e9915cf350ae66ad7b993edb6
ParadoxEngineer1 <jedwards@vtechsupport.com>
2025-03-27 14:38:41 -0700

    Update RenameWorkstation.ps1
    v0.1.2 Added Debugging Stuff

--- Basic Scripts/RenameWorkstation.ps1
+++ Basic Scripts/RenameWorkstation.ps1
@@ -1,9 +1,21 @@
+#RenameWorkstation.ps1
+#v0.1.2 - 3/27/2025
+#Created by Jonathan Edwards
+#Renames workstation for Dell computers based on laptop v desktop and serialnumber.
+
+
 #Rename Workstation
 $serialTag = (Get-WmiObject -class win32_bios).SerialNumber
 
 if (((Get-Computerinfo).CsPCSystemType) -eq "Desktop") {
-  Rename-Computer -NewName ("D" + $serialTag + "-D")
+	Rename-Computer -NewName ("D" + $serialTag + "-D")
+
+  	#TEST
+  	Set-Content -Path "C:\Deployment\RenameWorkstationAttempt.txt" -Value ("We tried to rename the workstation: D" + $serialTag + "-D")
 }
 else {
 	Rename-Computer -New Name ("D" + $serialTag + "-L")
+
+ 	#Test
+  	Set-Content -Path "C:\Deployment\RenameWorkstationAttempt.txt" -Value ("We tried to rename the workstation: D" + $serialTag + "-L")
 }
