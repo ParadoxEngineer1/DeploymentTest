c48f91eba3443f1413130c7414cefe76549eb2b5
ParadoxEngineer1 <jedwards@vtechsupport.com>
2025-03-27 13:22:48 -0700

    Update ScheduledTaskCreation.ps1
    v0.1.3 Added scheduled task creation and downloading LoginScript.ps1.

--- ScheduledTaskCreation.ps1
+++ ScheduledTaskCreation.ps1
@@ -1,9 +1,9 @@
 #StartUpScript.ps1
-#v0.1.2 - 3/27/2025
+#v0.1.3 - 3/27/2025
 #Created by Jonathan Edwards
 #Creates Scheduled Task that run on system startup.
 
-#Create Scheduled Task
+#Create Deployment Scheduled Task
 $action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"C:\StartUpScript.ps1`""
 $trigger = New-ScheduledTaskTrigger -AtStartup
 $principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
@@ -11,8 +11,19 @@ $settings = New-ScheduledTaskSettingsSet
 $task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
 Register-ScheduledTask Deployment -InputObject $task
 
+#Create Login Schedule Task
+$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"C:\LoginScript.ps1`""
+$trigger = New-ScheduledTaskTrigger -AtLogin
+$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+$settings = New-ScheduledTaskSettingsSet
+$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
+Register-ScheduledTask DeploymentLogin -InputObject $task
+
 #Download StartUpScript.ps1
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/ParadoxEngineer1/DeploymentTest/refs/heads/main/StartUpScript.ps1 -OutFile "C:\StartUpScript.ps1"
+
+#Download LoginScript.ps1
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/ParadoxEngineer1/DeploymentTest/refs/heads/main/LoginScript.ps1 -OutFile "C:\LoginScript.ps1"
 
 #Run StartUpScript.ps1
 & "C:\StartUpScript.ps1"
