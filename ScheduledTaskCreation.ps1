91eba0867592602fa1978306e584b5d6c9f7ee0d
ParadoxEngineer1 <jedwards@vtechsupport.com>
2025-03-28 11:53:04 -0700

    Update ScheduledTaskCreation.ps1
    More testing

--- ScheduledTaskCreation.ps1
+++ ScheduledTaskCreation.ps1
@@ -3,13 +3,30 @@
 #Created by Jonathan Edwards
 #Creates Scheduled Task that run on system startup. This file is run by the ppkg file.
 
+# Ensure Task Scheduler service is set to start automatically
+$serviceName = "Schedule"
+$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+if ($service.StartType -ne 'Automatic') {
+   Set-Service -Name $serviceName -StartupType Automatic
+}
+# Start Task Scheduler service if not running
+if ($service.Status -ne 'Running') {
+   Start-Service -Name $serviceName
+}
+
+Set-Content -Path "C:\ScheduleTaskStuff.txt" -Value $service.Status
+
+$service = Get-Service -Name $serviceName -ErrorAction SilentlyContinue
+
+Set-Content -Path "C:\ScheduleTaskStuff1.txt" -Value $service.Status
+
 #Create Deployment Scheduled Task
-$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"C:\StartUpScript.ps1`""
-$trigger = New-ScheduledTaskTrigger -AtStartup
-$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
-$settings = New-ScheduledTaskSettingsSet
-$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
-Register-ScheduledTask Deployment -InputObject $task
+#$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"C:\StartUpScript.ps1`""
+#$trigger = New-ScheduledTaskTrigger -AtStartup
+#$principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+#$settings = New-ScheduledTaskSettingsSet
+#$task = New-ScheduledTask -Action $action -Principal $principal -Trigger $trigger -Settings $settings
+#Register-ScheduledTask Deployment -InputObject $task
 
 #Create Login Schedule Task
 #$action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-File `"C:\LoginScript.ps1`""
